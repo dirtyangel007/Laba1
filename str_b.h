28d163494e516a21b6d8ba979b497f4b3a5ced6f
ElaineGwynbleidd <mih.nikita2006@gmail.com>
2025-03-12 00:54:19 +0700

    new file
    git

--- str_b.h
+++ str_b.h
@@ -1,3 +1,21 @@
-/c/Users/Admin/Downloads/str_b.h
+#include <string>
 
+std::string toUpperCase(std::string input)
+{
+	for (int i = 0; i < input.length(); i++)
+	{
+		if (input[i] >= 'a' && input[i] <= 'z')
+			input[i] = input[i] - 32;
+	}
+	return input;
+}
+
+std::string toLowerCase(std::string input)
+{
+	for (int i = 0; i < input.length(); i++)
+	{
+		if (input[i] >= 'A' && input[i] <= 'Z')
+			input[i] = input[i] + 32;
+	}
+	return input;
 
