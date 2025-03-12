228fe88ec6a6c6415bcf20c7cf5b648ed7a7cfaf
Konnesain <200383445+Konnesain@users.noreply.github.com>
2025-03-12 09:19:31 +0700

    da

--- main.cpp
+++ main.cpp
@@ -58,12 +58,5 @@ int main()
         }
         }
     }
-    cout << "¬ведите";
-    string vowelsTest = "aEoIssdu";
-    cout << "vowels count test: input - " << vowelsTest << "\tresult: " << vowelsCount(vowelsTest) << "\n";
-    string caseTest = "asdfzASDFZ";
-    cout << "case test: input - " << caseTest << "\tupper - " << toUpperCase(caseTest) << "\tlower - " << toLowerCase(caseTest) << "\n";
-    string palindromeTest = "asdffdsa";
-    cout << "palindrome test: input - " << palindromeTest << "\tresult - " << boolalpha << isPalindrome(palindromeTest);
     return 1;
 }
